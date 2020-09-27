c618404ce8c95f9e57b4c96ca23dc1c07d8a4bc9
akhvn <71459595+akhvn@users.noreply.github.com>
2020-09-27 19:23:21 +0600

    Delete 6.py

--- w1/6.py
+++
@@ -1,8 +0,0 @@
-A = {i for i in range(0,20,2)}
-B = {i for i in range(0,20,3)}
-print(A)
-print(B)
-print("Пересечение = ", A&B)
-print("Объединение = ", A|B)
-print("Симметрическая разность = ", A^B)
-
