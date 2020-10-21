74e80ac7953af8a019772d706e3ffda934a31331
akhvn <71459595+akhvn@users.noreply.github.com>
2020-10-22 00:37:00 +0600

    HW_5

---
+++ w5/mother.py
@@ -0,0 +1,26 @@
+class Mother:
+	def __init__(self, name):
+		self.__name = name
+
+	def get_name(self):
+		return self.__name
+
+	def __str__(self):
+		return "Name: {}".format(self.get_name())
+
+class Daughter(Mother):
+	def __init__(self, name, mother):
+		super().__init__(name)
+		self.__mother = mother
+
+	def get_mother(self):
+		return self.__mother
+
+	def __str__(self):
+		return "Name: {}, Mother: {}".format(self.get_name(), self.get_mother())
+
+Amily = Mother("Amily")
+Alina = Daughter("Alina", "Amily")
+
+print(Amily)
+print(Alina)
