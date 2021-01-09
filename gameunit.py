5a840538d37139f623a1bba3ad4e1bab6fd9e7b1
ARBaranov <71657162+ARBaranov@users.noreply.github.com>
2021-01-09 19:04:57 +0300

    Update gameunit.py

--- gameunit.py
+++ gameunit.py
@@ -13,16 +13,3 @@ class Attacker:
 
     def is_alive(self):
         return self._health > 0
-    
-    def add_experience(self):
-        self._experience = self._experience + 25
-        if self._experience >= 0 and self._experience <= 99:
-            self._lvl = 1
-        elif self._experience >= 100 and self._experience <= 199:
-            self._lvl = 2
-        elif self._experience >= 200 and self._experience < 299:
-            self._lvl = 3
-        elif self._experience >= 300 and self._experience <= 399:
-            self._lvl = 4
-        elif self._experience >= 400 and self._experience <= 500:
-            self._lvl = 5
