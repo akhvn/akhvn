ba558cb8fee907b2ae3fca6c6be0882c7990b0f0
ARBaranov <71657162+ARBaranov@users.noreply.github.com>
2021-01-09 14:52:32 +0300

    Update hero.py

--- hero.py
+++ hero.py
@@ -9,6 +9,7 @@ from gameunit import *
 """
 
 class Wizard(Attacker):
+    _lvl = 1
     def __init__(self):
         self._health = 75
         self._attack = 125
@@ -16,6 +17,7 @@ class Wizard(Attacker):
         self._name = name
         self._inportance = 1.5
 class Healer(Attacker):
+    _lvl = 1
     def __init__(self):
         self._health = 50
         self._attack = 50
@@ -23,6 +25,7 @@ class Healer(Attacker):
         self._name = name
         self._inportance = 0.5
 class Jagernaut(Attacker):
+    _lvl = 1
     def __init__(self):
         self._health = 125
         self._attack = 60
@@ -30,6 +33,7 @@ class Jagernaut(Attacker):
         self._name = name
         self._inportance = 1.15
 class Imba(Attacker):
+    _lvl = 1
     def __init__(self):
         self._health = 1000
         self._attack = 1000
